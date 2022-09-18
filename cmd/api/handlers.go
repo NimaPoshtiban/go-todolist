b0aeac3ded820e0b6a65770c6fa218158eaa6d00
Nima Poshtiban <Poshtibannima@gmail.com>
2022-09-18 16:53:53 +0430

    Add Swagger API and GET for getting one task by id

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+	// "golang.org/x/net/idna"
+	//  "github.com/swaggo/swag/example/celler/httputil"
+	//  "github.com/swaggo/swag/example/celler/model"
+	// // "github.com/swaggo/files"
 )
 
+// statusHandler godoc
+// @Summary     Show server status
+// @Description show server status
+// @Tags        status
+// @Produce json
+
+// @Success 200
+
+// @Router /status [get]
 func (app *application) statusHandler(w http.ResponseWriter, r *http.Request) {
 	currentStatus := appStatus{
 		Status:      "Available",
@@ -21,4 +38,39 @@ func (app *application) statusHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(js)
+}
+
+
+// statusHandler godoc
+// @Summary     Get Task
+// @Description Get the task by id
+// @Tags        task
+// @Param        id   path      int  true  "Task ID"
+// @Accept       json
+// @Produce      json
+// @Produce json
+// @Success      200  {object}   models.Task
+// @Failure      400  {object}  httputil.HTTPError
+// @Failure      404  {object}  httputil.HTTPError
+// @Failure      500  {object}  httputil.HTTPError
+// @Router  /tasks/{id} [get]
+func (app *application) getOneTask(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+
+	if err != nil {
+		app.logger.Print(errors.New("Invalid Id parameter"))
+		app.errorJSON(w, err)
+		return
+	}
+
+	task, err := app.models.DB.Get(id)
+	app.logger.Print(task)
+	err = app.writeJSON(w, http.StatusOK, &task, "task")
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"github.com/julienschmidt/httprouter"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
-func (app *application) routes() *httprouter.Router{
+func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
-	router.HandlerFunc(http.MethodGet,"/status",app.statusHandler)
-
+	router.HandlerFunc(http.MethodGet, "/api/v1/status", app.statusHandler)
+	router.HandlerFunc(http.MethodGet, "/api/v1/tasks/:id", app.getOneTask)
+	router.HandlerFunc(http.MethodGet, "/swagger/*any", httpSwagger.Handler(httpSwagger.URL("http://localhost:4000/swagger/doc.json")))
 	return router
 }
